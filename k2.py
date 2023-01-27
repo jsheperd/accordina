5bc7770f422062901f42fca0a723ccc82e9645d1
János JUHÁSZ <janos.juhasz@i-new.com>
2023-01-27 13:11:46 +0100

    choose card

--- k2.py
+++ k2.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 
 import pygame
 import pygame.midi
 import sys
+from goto import label, goto
 
 #                init
 # =======================================
@@ -14,14 +15,17 @@ pygame.midi.init()
 print(pygame.midi.get_device_info(2))
 midi_out = pygame.midi.Output(2)
 midi_out.set_instrument(instrument)
+music = True
 # =======================================
 def exit():
     global midi_out, music
 
-    music = 0
-    del midi_out
+    music = False
     pygame.midi.quit()
-    pygame.quit()
+    del midi_out
+    print("still ok")
+    #pygame.quit()
+    print("it is missing")
     sys.exit()
 
 mapping={ 94: 74, 38: 75, 25: 76, 12: 77,  
@@ -37,25 +41,46 @@ mapping={ 94: 74, 38: 75, 25: 76, 12: 77,
           61: 104, 48: 105, 35: 106, 22: 107
         }
 
+
+
+mapping={  100:  62,  4:   63,  26:  64,  32:  65,
+           29:   65,  22:  66,  8:   67,  33:  68,
+           27:   68,  7:   69,  21:  70,  34:  71,
+           6:    71,  9:   72,  23:  73,  35:  74,
+           25:   74,  10:  75,  28:  76,  36:  77,
+           5:    77,  11:  78,  24:  79,  37:  80,
+           17:   80,  13:  81,  12:  82,  38:  83,
+           16:   83,  14:  84,  18:  85,  39:  86,
+           54:   86,  15:  87,  19:  88,  45:  89,
+           55:   89,  51:  90,  47:  91,  46:  92,
+           56:   92,  52:  93,  48:  94,
+}
+
+
+        
+
 screen = pygame.display.set_mode((400, 400))
 volume = 120
 
-while True:
+while music:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
         if event.type == pygame.KEYDOWN:
             mapping.setdefault(event.scancode, 0)
-            note = mapping.get(event.scancode)-12
+            note = mapping.get(event.scancode)
             print(event)
             if note:
                 midi_out.note_on(note, volume) 
 
         if event.type == pygame.KEYUP:
             mapping.setdefault(event.scancode, 0)
-            note = mapping.get(event.scancode)-12
+            note = mapping.get(event.scancode)
             if note:
                 midi_out.note_off(note, volume) 
 
+            if event.key == 27:
+                exit()
             if event.key == pygame.K_ESCAPE:
                 exit()
+
